792d27dd3fc304f2c4230831ad709922680e5988
James Petts <jpetts@12136phydt.icr.ac.uk>
2019-07-09 16:11:00 +0100

    Doc sessionMap.js

--- Packages/icr-series-info-provider/client/lib/sessionMap.js
+++ Packages/icr-series-info-provider/client/lib/sessionMap.js
@@ -1,4 +1,13 @@
 const sessionMap = {
+  /**
+   * Returns the metadata for a scan, or just one property, if specified.
+   *
+   * @param {string} seriesInstanceUid  The seriesInstanceUid of the scan.
+   * @param {string} [property] A particular property to return.
+   *
+   * @returns {Object||string} An object if no property is specified, or the
+   *                           value of the specified property.
+   */
   getScan: (seriesInstanceUid, property) => {
     if (!seriesInstanceUid) {
       return _map.scans;
@@ -14,6 +23,17 @@ const sessionMap = {
 
     return scan[property];
   },
+
+  /**
+   * Extracts all of the scans contained in the JSON manifest in the sessionMap,
+   * and stores them as scan entries with seriesInstanceUid, seriesDescription,
+   * seriesNumber, and additional supplied metadata.
+   *
+   * @param {Object} json A JSON manifest to parse.
+   * @param {Object} metadata Metadata to add to the scans entries.
+   *
+   * @returns {null}
+   */
   setScan: (json, metadata) => {
     console.log(json);
 
@@ -33,13 +53,28 @@ const sessionMap = {
         });
       }
     }
-
-    console.log(`end of sessionMap.set():`);
-    console.log(sessionMap);
   },
+
+  /**
+   * Adds metadata to the sessions metadata array.
+   *
+   * @param {Object} metadata Metadata to add to the session entries.
+   *
+   * @returns {null}
+   */
   setSession: metadata => {
     _map.sessions.push(metadata);
   },
+
+  /**
+   * Returns the metadata for a session, or just one property, if specified.
+   *
+   * @param {string} experimentId The experimentId of the session.
+   * @param {string} [property] A particular property to return.
+   *
+   * @returns {Object||string} An object if no property is specified, or the
+   *                           value of the specified property.
+   */
   getSession: (experimentId, property) => {
     if (!experimentId) {
       return _map.sessions;
@@ -55,27 +90,83 @@ const sessionMap = {
 
     return session[property];
   },
+
+  /**
+   * Sets the subjectId of the view.
+   *
+   * @param {string} subjectId The subjectId of the view.
+   *
+   * @returns {null}
+   */
   setSubject: subjectId => {
     _map.subject = subjectId;
   },
+
+  /**
+   * Returns the subjectId of the view.
+   *
+   * @returns {string}
+   */
   getSubject: () => {
     return _map.subject;
   },
+
+  /**
+   * Sets the projectId of the view.
+   *
+   * @param {string} projectId The projectId of the view.
+   *
+   * @returns {null}
+   */
   setProject: projectId => {
     _map.project = projectId;
   },
+
+  /**
+   * Returns the projectId of the view.
+   *
+   * @returns {string}
+   */
   getProject: () => {
     return _map.project;
   },
+
+  /**
+   * Sets the parentProjectId of the view.
+   *
+   * @param {string} parentProjectId The parentProjectId of the view.
+   *
+   * @returns {null}
+   */
   setParentProject: parentProjectId => {
     _map.parentProject = parentProjectId;
   },
+
+  /**
+   * Returns the parentProjectId of the view.
+   *
+   * @returns {string}
+   */
   getParentProject: () => {
     return _map.parentProject;
   },
+
+  /**
+   * Sets the experimentId of the view.
+   *
+   * @param {string} experimentId The experimentId of the view.
+   *
+   * @returns {null}
+   */
   setExperiment: experimentId => {
     _map.experiment = experimentId;
   },
+
+  /**
+   * Returns the experimentId of the view.
+   *
+   * @returns {string}
+   */
   getExperiment: () => {
     return _map.experiment;
   }
@@ -89,8 +180,5 @@ _map = {
   parentProject: "",
   experiment: ""
 };
-
-console.log("SESSION_MAP:");
-console.log(_map);
 
 export { sessionMap };
