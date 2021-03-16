26cd851bc53ff130e8c51e6d58aa89108083c2a4
Mo Alsad <mo.alsad@gmail.com>
2021-03-16 21:23:53 +0000

    Remove all Meteor-based viewer files

--- Packages/ohif-lesiontracker/client/compatibility/index.js
+++
@@ -1,4 +0,0 @@
-import './imageDownload.js';
-// TODO: Migrate and create both tools on top of cornerstoneTools v3
-//import './deleteLesionKeyboardTool.js';
-//import './qualitativeTargetTools.js';
