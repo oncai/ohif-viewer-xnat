dcb9dce33f00db21616beb168ac3300c218a2063
Mo Alsad <mo.alsad@gmail.com>
2021-09-13 00:32:59 +0100

    notify the user that the XNAT session has timed out

--- platform/viewer/src/components/OHIFLogo/OHIFLogo.js
+++ platform/viewer/src/components/OHIFLogo/OHIFLogo.js
@@ -1,44 +1,57 @@
-import './OHIFLogo.css';
-
 import { Icon } from '@ohif/ui';
 import React from 'react';
 import { useIdleTimer } from 'react-idle-timer';
 import { userManagement } from '@xnat-ohif/extension-xnat';
 
-const timeout = 1000 * 60 * 10;
+import './OHIFLogo.css';
+
+// const KeepAliveTimeout = 1000 * 60 * 0.5;
 let timeSinceLastApi = 0;
 
 function OHIFLogo() {
+  const sessionTimeout = userManagement.getXNATSessionTimeout();
+  const KeepAliveTimeout = Math.floor(sessionTimeout * 0.25);
+
   const handleOnAction = () => {
-    if (timeSinceLastApi >= timeout) {
+    if (timeSinceLastApi >= KeepAliveTimeout) {
+      timeSinceLastApi = 0;
+      reset();
       // invoke api call
-      userManagement.getSessionID()
+      userManagement
+        .getSessionID()
         .then(data => {
           console.log('Keeping the XNAT session alive...');
         })
         .catch(error => {
           console.warn(error);
         });
-      timeSinceLastApi = 0;
-      reset();
     } else {
       timeSinceLastApi = getElapsedTime();
     }
-  }
+  };
 
-  const { getElapsedTime, reset } = useIdleTimer({
-    timeout: timeout,
+  const handleOnIdle = event => {
+    console.log('user is idle', event);
+    console.log('last active', getLastActiveTime());
+    console.log('##### Logged out!');
+    userManagement.logUserOut();
+  };
+
+  const { getElapsedTime, reset, getLastActiveTime } = useIdleTimer({
+    timeout: sessionTimeout,
     events: [
       'keydown',
       'wheel',
+      // 'mousemove',
       'mousewheel',
       'mousedown',
       'touchstart',
-      'touchmove'
+      'touchmove',
     ],
     onAction: handleOnAction,
-    startOnMount: false,
-    debounce: 500
+    onIdle: handleOnIdle,
+    startOnMount: true,
+    debounce: 500,
   });
 
   let versionStr = '';
@@ -46,41 +59,30 @@ function OHIFLogo() {
   if (version) {
     versionStr = `v${version.major}.${version.minor}.${version.patch}`;
     if (version.dev) {
-      versionStr += `-${version.dev}`
+      versionStr += `-${version.dev}`;
     }
     if (version.build) {
-      versionStr += ` build-${version.build}`
-    }
-    if (version.dev) {
-      versionStr += ' | INVESTIGATIONAL USE ONLY';
+      versionStr += ` build-${version.build}`;
     }
   }
-
 
   return (
     <a
       target="_blank"
-      // rel="noopener noreferrer"
+      rel="noopener noreferrer"
       className="header-brand"
-      // href="http://ohif.org"
+      href="https://bitbucket.org/icrimaginginformatics/ohif-viewer-xnat-plugin"
     >
       <Icon name="xnat-ohif-logo" className="header-logo-image" />
       <Icon name="xnat-icr-logo" className="header-logo-image-icr" />
       <div className="header-logo-text">
-        OHIF-XNAT Viewer <span style={{ color: '#91b9cd', fontSize: 13 }}>|{` ${versionStr}`}</span>
-
+        OHIF-XNAT Viewer{' '}
+        <span style={{ color: '#91b9cd', fontSize: 13 }}>
+          |{` ${versionStr}`}
+        </span>
       </div>
-      {/*<Icon name="ohif-logo" className="header-logo-image" />*/}
-      {/* Logo text would fit smaller displays at two lines:
-       *
-       * Open Health
-       * Imaging Foundation
-       *
-       * Or as `OHIF` on really small displays
-       */}
-      {/*<Icon name="ohif-text-logo" className="header-logo-text" />*/}
     </a>
   );
 }
 
-export default OHIFLogo;
+export default React.memo(OHIFLogo);
