aa5056a63f836179b885436a455170194f294e4b
Mo Alsad <m.alsad@imperial.ac.uk>
2022-02-28 18:56:14 +0000

    Fix contour ROI import/export for multiframe images (Issue #29)

--- extensions/xnat/src/utils/IO/classes/RoiImporter.js
+++ extensions/xnat/src/utils/IO/classes/RoiImporter.js
@@ -240,7 +240,7 @@ export default class RoiImporter {
         for (let k = 0; k < images.length; k++) {
           const image = images[k];
           const sopInstanceUID = image.getSOPInstanceUID();
-          const imageId = image.getImageId();
+          const imageId = image.getData().url; //image.getImageId();
 
           sopInstanceUidToImageIdMap[sopInstanceUID] = imageId;
         }
